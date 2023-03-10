dcc40a3225ca948dd216512de753b31b7feea151
isobelmorales <isofmorales@gmail.com>
2023-03-10 14:15:26 -0500

    Completed Deliverable

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.color} {self.name}'
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    habitat = models.CharField(max_length=100)
+    food = models.CharField(max_length=100)
+    nesting = models.CharField(max_length=100)
+    behavior = models.CharField(max_length=100)
+    conservation = models.CharField(max_length=100)
+    toys = models.ManyToManyField(Toy)
+
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+    # dunder str method to return finch name
+    def __str__(self):
+        return self.name
+    
+    # direct to the detail view for a resource
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id })
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        #add the choices field
+        choices = MEALS,
+        # set a default value for the meal to be 'B'
+        default = MEALS[0][0]
+    )
+    # add a finch foreign key reference
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    # change the default sort
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
-# finches dictionary ("seed")
-finches = [
-    {'name': 'American Goldfinch', 'habitat': 'Open Woodlands', 'food': 'Seeds', 'nesting': 'Shrub', 'behavior': 'Foliage Gleaner', 'conservation': 'Low Concern'},
-    {'name': 'Black Rosy-Finch', 'habitat': 'Tundra', 'food': 'Seeds', 'nesting': 'Ground', 'behavior': 'Ground Forager', 'conservation': 'Red Watch List'}
-]
+# Import Finch model
+from .models import Finch, Toy
+from .forms import FeedingForm
 
 # Define the home view
 def home(request):
@@ -16,6 +17,88 @@ def about(request):
 
 # Define the finches index view
 def finches_index(request):
-    return render(request, 'finches/index.html', {
-        'finches': finches
-    })
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', {'finches': finches})
+
+# Finches detail route
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+
+     # first we'll get a list of ids of toys the finch owns
+    id_list = finch.toys.all().values_list('id')
+    # then we'll make a list of the toys the finch does not have
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+
+    return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have })
+
+# Create finch view
+class FinchCreate(CreateView):
+    model = Finch
+    fields='__all__'
+
+# Update Finch
+class FinchUpdate(UpdateView):
+    model = Finch
+    # let's use custom fields to disallow renaming a finch
+    fields = ['habitat', 'food', 'nesting', 'behavior', 'conservation']
+
+# Delete Finch
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+
+# Add Feeding
+def add_feeding(request, finch_id):
+    # create a ModelForm instance from the data in request.POST
+    form = FeedingForm(request.POST)
+
+    # we need to validate the form, that means "does it match our data?"
+    if form.is_valid():
+        # we dont want to save the form to the db until is has the finch id
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+def assoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+# ToyList
+class ToyList(ListView):
+    model = Toy
+    template_name = 'toys/index.html'
+
+# ToyDetail
+class ToyDetail(DetailView):
+    model = Toy
+    template_name = 'toys/detail.html'
+
+# ToyCreate
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+    # define what the inherited method is_valid does(we'll update this later)
+    def form_valid(self, form):
+        # we'll use this later, but implement right now
+        # we'll need this when we add auth
+        # super allows for the original inherited CreateView function to work as it was intended
+        return super().form_valid(form)
+
+# ToyUpdate
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+# ToyDelete
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
